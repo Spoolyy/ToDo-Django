a8a2f9230c48751052ad8a7bfa59a601fe0a5c18
Spoolyy <alessiosimeone3@gmail.com>
2024-10-11 20:35:21 +0200

    temporary committ

--- mysite/ToDo_List/views.py
+++ mysite/ToDo_List/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
+from django.utils import timezone
+from datetime import timedelta
 
 def index(request):
     tasks = Task.objects.all()
-    return render(request, "index.html", {"tasks":tasks})
+    current_time = timezone.now()
+    one_day_from_now = current_time + timedelta(days=1)
+    return render(request, "index.html", {"tasks":tasks, "current_time":current_time, "one_day_from_now":one_day_from_now})
 
+def create(request):
+    if request.method == "POST":
+        pass
