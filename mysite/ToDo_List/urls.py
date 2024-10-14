aed42f80adae609b8fcf7e285c76db7fd04b8c29
Spoolyy <alessiosimeone3@gmail.com>
2024-10-14 21:36:54 +0200

    Final Commit
    Project completed

--- mysite/ToDo_List/urls.py
+++ mysite/ToDo_List/urls.py
@@ -5,5 +5,7 @@ app_name = "todo-list"
 
 urlpatterns = [
     path("task/", views.index, name="index"),
-    path("task/createnew", views.create, name="create")
+    path("task/createnew", views.create, name="create"),
+    path("task/<int:pk>/edit", views.update, name="update"),
+    path("task/<int:pk>/delete", views.delete, name="delete")
 ]
--- mysite/ToDo_List/views.py
+++ mysite/ToDo_List/views.py
@@ -20,3 +20,21 @@ def create(request):
     else :
         form = TaskForm()
     return render(request, "create.html", {"form":form})
+
+def update(request, pk):
+    task = get_object_or_404(Task, pk = pk)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("/todolist/task")
+    else:
+        form = TaskForm(instance=task)
+    return render(request, "update.html", {"form":form})
+
+def delete(request, pk):
+    task = get_object_or_404(Task, pk = pk)
+    if request.method == "POST":
+        task.delete()
+        return redirect("/todolist/task")
+    return render(request, "delete.html", {"task":task})
